64da71da7c12c0a0cc979dd834b8769b1243bbff
EcoByteBasher <ecobytebasher@gmail.com>
2026-07-22 09:30:35 +0100

    Layout tweak

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
 const APP_NAME      = "Puzzle Assistant";
-const APP_VERSION   = "1.0.2";
+const APP_VERSION   = "1.0.3";
 const APP_AUTHOR    = "Chris Hackman";
 const APP_YEAR      = "2026";
 const APP_COPYRIGHT = `© ${APP_YEAR} ${APP_AUTHOR}`;
